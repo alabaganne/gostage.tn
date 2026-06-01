57168c5aa4dbd48645c72c8bf7733e556461af21
Ala Baganne <alabaganne9@gmail.com>
2026-06-01 14:33:41 +0100

    Undo all design changes and fix like internship feature

--- resources/js/mixins/like.js
+++ resources/js/mixins/like.js
@@ -1,19 +1,11 @@
+import { Inertia } from '@inertiajs/inertia';
+
 export default {
-	emits: ['dislike'],
-	methods: {
-		toggleLike(internship) {
-			axios.post(route('likes.store', internship))
-				.then(() => {
-					if(internship.liked) {
-						internship.liked = false;
-						this.currentUser.likes_count--;
-						this.$emit('dislike', internship);
-					} else {
-						internship.liked = true;
-						this.currentUser.likes_count++;
-					}
-				})
-				.catch(error => console.log(error));
-		}
-	}
-}
+    methods: {
+        toggleLike(internship) {
+            Inertia.post(route('likes.store', internship.id), {}, {
+                preserveScroll: true,
+            });
+        }
+    }
+};
